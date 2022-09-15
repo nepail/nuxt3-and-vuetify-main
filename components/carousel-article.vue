f05c0b390656c93f8a1c3a036803ebc8d9e5eeb2
jim <jim@f1good.com>
2022-09-15 16:57:40 +0800

    temp

---
+++ components/carousel-article.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="hot-articles">
+        <div class="title-wrapper">
+            <div class="title" >
+                <h1 class="title-txt" >最新资讯</h1>
+                <h2 class="subtitle-txt" >让您轻松掌握热门市场动态</h2>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+.hot-articles {
+    .title-wrapper {
+        padding-top: 0;
+    }
+}
+.title {
+    display: -webkit-box;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    -webkit-line-clamp: 1;
+    -webkit-box-orient: vertical;
+    line-height: 1.4;
+}
+.title-wrapper {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 1rem;
+    padding-top: 2.25rem;
+    padding-bottom: 10px;
+    width: 100%;
+    border-bottom: 3px solid #ee3324;
+    .title{
+        display: flex;
+        align-items: flex-end;
+        .title-txt{
+            font-size: 20px;
+            color: #ee3324;
+        }
+    }
+}
+
+.title-wrapper .title .subtitle-txt {
+    display: none;
+    padding-left: .5rem;
+    font-weight: 400;
+    color: #404040;
+    letter-spacing: .5px;
+}
+
+@media (min-width: 992px){
+    .title-wrapper{
+        .title{
+            .title-txt{
+                font-size: 26px;
+            }
+            .subtitle-txt{
+                display: block;
+            }
+        }
+    }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,28 +3,45 @@
         <div class="homepage-main">
             <prop-cycle></prop-cycle>
             <div class="grid-area">
+                <div class="left-grid-area">
+                    <carousel-article></carousel-article>
+                </div>
+                <div class="right-grid-area">
 
+                </div>
             </div>
         </div>
     </div>
 </template>
 
-<style>
-    .homepage{
-        display: flex;
-        padding: 0 20px;
-        width: 100%;
-        flex-direction: column;
-    }
+<style lang="scss">
+.homepage {
+    display: flex;
+    padding: 0 20px;
+    width: 100%;
+    flex-direction: column;
 
-    .homepage .homepage-main{
+
+}
+
+@media (min-width: 992px) {
+    .homepage-main {
         margin: auto;
         padding: 0;
         width: 100%;
         max-width: 1200px;
+        .grid-area{
+            display: flex;
+            padding-top: 2rem;
+            flex-wrap: wrap;
+            column-gap: 60px;
+        }
+        .left-grid-area{
+            width: calc(66% - 30px);
+        }
+        .right-grid-area{
+            width: calc(33% - 30px);
+        }
     }
-
-    .grid-area{
-        
-    }
+}
 </style>

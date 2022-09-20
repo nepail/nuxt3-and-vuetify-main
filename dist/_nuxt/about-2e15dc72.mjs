22f06e13696c59216fd877523ea2fdcda970d99f
Lin Nepail <kls18z@hotmail.com>
2022-09-20 19:28:26 +0800

    Build dist

---
+++ dist/_nuxt/about-2e15dc72.mjs
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as o}from"./entry-bff167c0.mjs";const r={};function t(a,n){return c(),o("div",null,"about")}var s=e(r,[["render",t]]);export{s as default};
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  ssr: true,
+  ssr: false,
   app:{
     head:{
       title: 'esball',
